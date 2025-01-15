9b4653fff3dd2b62662b2baa8ac4bfb23d428f8c
Rohald van Merode <rohald_89@live.nl>
2025-01-15 16:59:08 +0100

    adjusted 'epic notes' mentions to new project name

--- app/root.tsx
+++ app/root.tsx
@@ -76,7 +76,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction<typeof loader> = ({ data }) => {
 	return [
-		{ title: data ? 'Epic Notes' : 'Error | Epic Notes' },
+		{ title: data ? 'Zenith' : 'Error | Zenith' },
 		{ name: 'description', content: `Your own captain's log` },
 	]
 }
